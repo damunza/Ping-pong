3015045f5bb7ad12bab0c44763d36002de1af200
damunza <danidamunza@gmail.com>
2018-07-13 09:52:25 +0300

    entered the business logic part

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
   $('form#number').submit(function(event){
     event.preventDefault();
-    var input=parseInt($('input#numeral'));
+    var input=parseInt($('input#numeral').val());
+    var output=pingpong(input);
+    // alert(input);
+    // console.log(input);
   })
 })
+
+var pingpong=function(input){
+  for(var x=1; x<=input ; x++){
+    if (x % 3 ===0 && x % 5 === 0) {
+      console.log("pingpong");
+    }else if (x % 3 ===0) {
+      console.log("ping");
+    }else if (x % 5 ===0) {
+      console.log("pong");
+    }else {
+      console.log(x);
+    }
+  }
+
+  }
